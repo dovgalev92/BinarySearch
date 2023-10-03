f7511bcd3b9f8ade63d80a1d270fb8a95e45cb24
dovgalev92 <dovgalev2021@inbox.ru>
2023-10-04 00:30:20 +0300

    binarySearch

---
+++ Algoritm(BinarySearch)/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Простейший бинарный поиск");
+
+int number = int.Parse(Console.ReadLine());
+int low = 0;
+int[] n = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+var high = n.Length-1;
+var res  = 0;
+
+while (low <= number)
+{
+    res = (low + high)/2;
+    var index = n[res];
+    if(index == number)
+    {
+        break;
+    }
+    else if(number < index)
+    {
+        high = res - 1;
+    }
+    else
+    {
+        low = res + 1;
+    }
+}
+Console.WriteLine($"позиция в массиве {res}, значение {n[res]}");
+
